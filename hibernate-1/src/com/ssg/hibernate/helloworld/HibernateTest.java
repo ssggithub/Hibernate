258d1796284cee0d88d4c79a5768d84c80f2f972
ssggithub <13691897490@163.com>
2017-02-12 21:30:55 +0800

    hibernate-1
    hibernate-1

---
+++ hibernate-1/src/com/ssg/hibernate/helloworld/HibernateTest.java
@@ -0,0 +1,49 @@
+package com.ssg.hibernate.helloworld;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.junit.Test;
+
+public class HibernateTest {
+
+	@Test
+	public void test() {
+		System.out.println("test starting...");
+
+		// 1.创建sessionFactory对象
+		SessionFactory sessionFactory = null;
+
+		// 2.创建Configuration对象，对应 hibernate 的基本配置信息和 对象关系映射信息
+		Configuration configuration = new Configuration().configure();
+
+		// 3.创建ServiceRegistry对象:hibernate 4.x 新添加的对象
+		// hibernate 的任何配置和服务都需要在该对象中注册后才能有效.
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties())
+				.buildServiceRegistry();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		
+		//4.创建session对象
+		Session session = sessionFactory.openSession();
+		
+		//5.开启事务
+		Transaction transaction = session.beginTransaction();
+		
+		//6.执行保存操作
+		News news = new News("hibernate-1", "ssg", new Date(new java.util.Date().getTime()));
+		session.save(news);
+		
+		//7.提交事务
+		transaction.commit();
+		
+		session.close();
+		sessionFactory.close();
+		
+		
+	}
+}
---
+++ hibernate-1/src/com/ssg/hibernate/helloworld/News.java
@@ -0,0 +1,86 @@
+package com.ssg.hibernate.helloworld;
+
+import java.sql.Blob;
+import java.util.Date;
+
+public class News {
+	private Integer id; //field
+	private String title;
+	private String author;
+	
+	private String conts;
+	
+	private Blob picture;
+	
+	public Blob getPicture() {
+		return picture;
+	}
+
+	public void setPicture(Blob picture) {
+		this.picture = picture;
+	}
+
+	public String getConts() {
+		return conts;
+	}
+
+	public void setConts(String conts) {
+		this.conts = conts;
+	}
+	
+
+	private Date dates;
+
+	
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getDates() {
+		return dates;
+	}
+
+	public void setDates(Date dates) {
+		this.dates = dates;
+	}
+
+	public News(String title, String author, Date dates) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.dates = dates;
+	}
+	
+	public News() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "News [id=" + id + ", title=" + title + ", author=" + author
+				+ ", date=" + dates + "]";
+	}
+
+}
