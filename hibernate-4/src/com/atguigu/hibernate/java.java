a8366a2e1dc693205015a9ae700e2ac051e73bd8
ssggithub <13691897490@163.com>
2017-02-16 23:39:24 +0800

    hibernate-4

--- hibernate-4/src/com/atguigu/hibernate/java.java
+++
@@ -1,5 +0,0 @@
-package com.atguigu.hibernate;
-
-public class java {
-
-}
---
+++ hibernate-4/src/com/ssg/hibernate/utils/HibernateUtils.java
@@ -0,0 +1,35 @@
+package com.ssg.hibernate.utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtils {
+
+	public HibernateUtils() {
+	}
+
+	public static HibernateUtils instance = new HibernateUtils();
+
+	public static HibernateUtils getInstance() {
+		return instance;
+	}
+
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration configuration = new Configuration().configure();
+			ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties())
+					.buildServiceRegistry();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		}
+		return sessionFactory;
+	}
+
+	public Session getSession() {
+		return getSessionFactory().getCurrentSession();
+	}
+}
